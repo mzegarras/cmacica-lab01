51740f5782db713f550a93b11e68f5e8498be245
Manuel <mzegarra@gmail.com>
2018-12-01 06:05:47 -0500

    Version Inicial

---
+++ src/main/java/pe/cmacica/labs/labs01/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package pe.cmacica.labs.labs01.dto;
+
+public class UserDTO {
+
+
+    private String numero;
+    private String clave;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
