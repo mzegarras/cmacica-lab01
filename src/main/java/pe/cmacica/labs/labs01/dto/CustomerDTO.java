242967173f1746e9588bb1aa0a6123c242bdd758
Manuel <mzegarra@gmail.com>
2018-12-01 05:42:57 -0500

    Add dtos

---
+++ src/main/java/pe/cmacica/labs/labs01/dto/CustomerDTO.java
@@ -0,0 +1,32 @@
+package pe.cmacica.labs.labs01.dto;
+
+public class CustomerDTO {
+
+    private String codigo;
+    private String nombre;
+    private String apellidos;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
